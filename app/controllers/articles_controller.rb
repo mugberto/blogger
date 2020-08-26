e7865e76b953be9535fc3579834573a2985ea77e
Morrism1 <morrismontana0@gmail.com>
2020-08-26 11:03:24 +0200

    Create #article_helper method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
   def index
     @articles = Article.all
   end
@@ -12,8 +13,9 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(params[:article])
+    @article = Article.new(article_params)
     @article.save
+
     redirect_to article_path(@article)
   end
 end
