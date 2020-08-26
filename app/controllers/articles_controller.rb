f77eb50f163ab7614d8a9b5f3bb9cda32d6b65d3
Hubert MUGABO <mugberto@gmail.com>
2020-08-26 10:05:45 +0200

    Add #new method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,12 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
   def show 
     @article = Article.find(params[:id])
+  end
+
+  def new
+
   end
 end
